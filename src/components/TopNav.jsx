a8f9b2fa5663978beda1c9b78b6f0c2c5893a7c8
RivianRithick <rithickshival16@gmail.com>
2024-02-23 20:27:03 +0530

    New Commit

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import GuviLogo from "../assets/Guvi-blog-logo.png";
+import navimg1 from "../assets/blog-header.png";
+function TopNav() {
+  return (
+    <>
+      <nav className="navbar bg-body-tertiary topnav">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={GuviLogo} alt="Bootstrap" width="120" height="50" />
+          </a>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="navimg d-flex justify-content-center">
+          <img src={navimg1} alt="Bootstrap" width="90%" height="30%" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TopNav;
